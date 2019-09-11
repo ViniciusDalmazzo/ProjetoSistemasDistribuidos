3059e7fcec009fc904fa31df335dfd3eeed021bd
Vinicius Camargo <vinicius.camargo@uppertools.inf.br>
2019-09-11 08:55:57 -0300

    Ajustes no layout do form para implementação do Heartbeat

--- ChatUDP/Client/LoginForm.Designer.cs
+++ ChatUDP/Client/LoginForm.Designer.cs
@@ -33,14 +33,22 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.listView2 = new System.Windows.Forms.ListView();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
-            this.listView1.Enabled = false;
             this.listView1.Location = new System.Drawing.Point(6, 19);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(456, 197);
@@ -70,7 +78,6 @@
             // listView2
             // 
             this.listView2.Alignment = System.Windows.Forms.ListViewAlignment.Left;
-            this.listView2.Enabled = false;
             this.listView2.Location = new System.Drawing.Point(6, 19);
             this.listView2.Name = "listView2";
             this.listView2.Size = new System.Drawing.Size(456, 197);
@@ -79,13 +86,89 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(318, 468);
+            this.button1.Location = new System.Drawing.Point(486, 428);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 34);
+            this.button1.Size = new System.Drawing.Size(205, 34);
             this.button1.TabIndex = 14;
             this.button1.Text = "Iniciar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 317);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Porta";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 333);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 20);
+            this.textBox2.TabIndex = 17;
+            this.textBox2.Text = "6969";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 356);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Tempo de atualização";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 372);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(185, 20);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.Text = "1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 21);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Adicionar IP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // listView3
+            // 
+            this.listView3.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listView3.Location = new System.Drawing.Point(6, 77);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(191, 238);
+            this.listView3.TabIndex = 11;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.listView3);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(486, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(205, 401);
+            this.groupBox3.TabIndex = 24;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Configurações";
             // 
             // LoginForm
             // 
@@ -93,7 +176,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(492, 514);
+            this.ClientSize = new System.Drawing.Size(703, 476);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -104,6 +188,8 @@
             this.Text = "Heartbeat";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -114,6 +200,14 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ListView listView2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- ChatUDP/Client/LoginForm.cs
+++ ChatUDP/Client/LoginForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -11,6 +12,8 @@ namespace Client
         private List<string> listaIps = new List<string>();
         private const string MensagemPadraoEnvio = "Heartbeat";
         private const string MensagemPadraoReceber = "Heartbeat recebido: ";
+        private int Porta;
+        private int TempoAtualizacao;
 
         public LoginForm()
         {
@@ -27,17 +30,28 @@ namespace Client
             listView2.Columns.Add("Name");
             listView2.Columns[0].Width = this.listView2.Width - 4;
             listView2.HeaderStyle = ColumnHeaderStyle.None;
-                      
+
+            listView3.Items.Clear();
+            listView3.View = View.Details;
+            listView3.Columns.Add("Name");
+            listView3.Columns[0].Width = this.listView2.Width - 4;
+            listView3.HeaderStyle = ColumnHeaderStyle.None;
+
+            InicializarInformacoesHeartbeat();
         }
 
         public void InicializarInformacoesHeartbeat()
         {
-            listaIps.Add("192.168.1.1");
-            listaIps.Add("192.168.1.2");
-            listaIps.Add("192.168.1.3");
-            listaIps.Add("192.168.1.4");
-            listaIps.Add("192.168.1.5");
-            listaIps.Add("192.168.1.6");
+            listView3.Items.Add("192.168.1.1");
+            listView3.Items.Add("192.168.1.2");
+            listView3.Items.Add("192.168.1.3");
+            listView3.Items.Add("192.168.1.4");
+            listView3.Items.Add("192.168.1.5");
+            listView3.Items.Add("192.168.1.6");
+
+            RecuperaIPSConfigurados();
+            RecuperaPortaConfigurada();
+            RecuperaTempoDeAtualizacao();
         }
 
         delegate void EscreveMensagemRecebe(string texto);
@@ -81,20 +95,56 @@ namespace Client
             {
                 foreach (var ip in listaIps)
                 {
-                    Udp.Send(MensagemPadraoEnvio, ip, 6969);
+                    Udp.Send(MensagemPadraoEnvio, ip, Porta);
 
                     Invoke(new EscreveMensagemEnvio(EscreverMensagemEnvio), $"Heartbeat enviado para: {ip}");
                 }
 
-                Thread.Sleep(1000);
+                Thread.Sleep(TempoAtualizacao);
             }
         }
 
         public void button1_Click(object sender, EventArgs e)
         {
             Udp = new UDP();
+            AlteraStatusDasConfiguracoes(false);
             InicializarInformacoesHeartbeat();
-            InitThread();
+            InitThread();            
+        }
+
+        public void RecuperaIPSConfigurados()
+        {
+            listaIps = listView3.Items.Cast<ListViewItem>()
+                                   .Select(item => item.Text)
+                                   .ToList();
+        }
+
+        public void RecuperaPortaConfigurada()
+        {
+            var porta = textBox2.Text;
+
+            Porta = Convert.ToInt32(porta);
+        }
+
+        public void RecuperaTempoDeAtualizacao()
+        {
+            var tempo = textBox3.Text;
+
+            TempoAtualizacao = Convert.ToInt32(tempo) * 1000;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var ip = textBox1.Text;
+
+            listView3.Items.Add(ip);
+
+            textBox1.Text = string.Empty;
+        }
+
+        private void AlteraStatusDasConfiguracoes(bool ativo)
+        {
+            groupBox3.Enabled = ativo;
         }
     }
 }
